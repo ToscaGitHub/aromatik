700adad4faf71cc30af47455276d79ed2cf0e684
MichMichdu13 <ugoperretd@gmail.com>
2021-03-15 13:32:52 +0100

    j'ai update l'admin

---
+++ admin.php
@@ -0,0 +1,9 @@
+<?php include("includes/init.inc.php");
+session_start();
+if (!empty($_SESSION["connect"])) {
+    if ($_SESSION["connect"] === true) {
+    } else {
+        header("Location: connexion.php");
+        exit;
+    }
+}
