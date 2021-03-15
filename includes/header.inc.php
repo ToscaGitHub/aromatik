f6da15c7c671d18cca048caecd0e536756248ca6
SCATOLINI Matteo <matteo.scatolini@my-digital-school.org>
2021-03-15 13:27:22 +0100

    Update

---
+++ includes/header.inc.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aromatik</title>
+
+    
+    <link rel="stylesheet" href="vendors/css/bootstrap.css">    
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/style.css">
+ 
+    <script src="vendors/js/bootstrap.js"></script>
+    <script src="js/jquery.js"></script>
+    <script src="js/main.js"></script>
+
+</head>
+<body>
+
+    <?php 
+
+        include("includes/init.inc.php"); 
+
+    ?>
+
+    <nav>
+
+        <div>
+
+            
+              
+        </div>
+
+
+    </nav>
